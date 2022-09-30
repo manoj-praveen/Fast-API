2b1a511c7a0a0a4fe7f73bdb851940c9874f4e58
manoj-praveen <manojpraveenkgm@gmail.com>
2022-09-30 10:41:44 +0530

    implemented oauth2 token

--- api/main.py
+++ api/main.py
@@ -1,10 +1,12 @@
 from api import models
 from api.database_connection import engine
+from api.routers.auth import auth_router
 from api.routers.posts import post_router
 from api.routers.users import user_router
 from app import app
 
 app.include_router(post_router)
 app.include_router(user_router)
+app.include_router(auth_router)
 
 models.Base.metadata.create_all(bind=engine)
---
+++ api/routers/auth.py
@@ -0,0 +1,29 @@
+from fastapi import status, Depends, APIRouter, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from api import models
+from api.database_connection import get_db
+from api.routers.oauth2 import create_access_token
+from utils.utils import verify_password
+
+auth_router = APIRouter(
+    prefix="/login", tags=['Authentication']
+)
+
+
+@auth_router.post("/", status_code=status.HTTP_201_CREATED)
+def login(request_payload: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email_id == request_payload.username).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid Credentials."
+        )
+    if not verify_password(request_payload.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Invalid Credentials."
+        )
+    access_token = create_access_token(data={"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer"}
