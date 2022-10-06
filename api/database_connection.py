d538b455147bb7023bf4140606aaff97084e9d6e
manoj-praveen <manojpraveenkgm@gmail.com>
2022-10-06 20:08:04 +0530

    added logic for environmental variables

--- api/database_connection.py
+++ api/database_connection.py
@@ -1,8 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:manoj1498@localhost/fast'
+from api.config import settings
+
+DB_USER = settings.DB_USER
+DB_PASSWORD = settings.DB_PASSWORD
+DB_HOST = settings.DB_HOST
+DB_NAME = settings.DB_NAME
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- api/routers/oauth2.py
+++ api/routers/oauth2.py
@@ -6,14 +6,15 @@ from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
 from api import models
+from api.config import settings
 from api.database_connection import get_db
 from api.schemas.request_schema import TokenData
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl='login')
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.SECRET_KEY
+ALGORITHM = settings.ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = int(settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
 
 def create_access_token(data: dict):
